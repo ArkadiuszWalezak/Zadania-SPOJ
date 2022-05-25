641264d3e7a5d7b010a105707b4fa20a66459eb1
Arek <arekwalezak@gmail.com>
2022-05-25 19:32:38 +0200

    new program

---
+++ Zadanie SPOJ 496 - dwie cyfry silni/Zadanie SPOJ 496 - dwie cyfry silni/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace Zadanie_SPOJ_496___dwie_cyfry_silni
+{
+    
+    //Program który liczy silnie. Pierwsze podaj ilość liczb do obliczenia, potem konkretne liczby do obliczeń.
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double y = 0;
+            int z = 0;
+            int l = 0;
+
+            double x = double.Parse(Console.ReadLine());
+            double[] n = new double [30];
+
+            while (y < x)
+            {
+                n[z] = double.Parse(Console.ReadLine());
+                z++;
+                y++;
+
+            }
+            
+            while (l < x)
+            {
+                Console.WriteLine(Mierzewa.Silnia(n[l]));
+                l++;
+            }
+            Console.ReadKey();
+        }
+        
+    }
+
+    public class Mierzewa
+    {
+        public static double Silnia(double liczba)
+        {
+            double n = 1;
+
+            for (double i = 1; i <= liczba; i++)
+            {
+                n = n * i;
+            }
+            liczba = n;
+            return liczba;
+        }
+    }
+}
