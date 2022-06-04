9c6696512ffa4f80010973ab5ae99d15de133094
Mateusyy <mtsz.grybos@gmail.com>
2022-06-04 18:06:54 +0200

    new exercise

---
+++ ZabawneDodawaniePiotrusia/ZabawneDodawaniePiotrusia/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ZabawneDodawaniePiotrusia
+{
+    class Program
+    {
+        static void Main()
+        {
+            int reverse;
+            int input = int.Parse(Console.ReadLine());
+
+            for ( ; ; )
+            {
+                string x = StringReverse(input);
+                reverse = int.Parse(x);
+
+                int result = reverse + input;
+                string resultString = result.ToString();
+                string resultStringReverse = StringReverse(result);
+
+                if (resultString == resultStringReverse)
+                {
+                    Console.WriteLine(resultString);
+                    return;
+                }
+                else
+                {
+                    input = result;
+                }
+            }
+
+        }
+
+        public static string StringReverse(int input)
+        {
+            string x = input.ToString();
+            char[] z = x.ToCharArray();
+            Array.Reverse(z);
+            string reversed = new string(z);
+            return reversed;
+        }
+    }
+}
