251818760b6d2e8006b53ee50606571aa43b39da
Mateusyy <mtsz.grybos@gmail.com>
2022-06-05 11:44:49 +0200

    new class project

---
+++ CounterProject/CounterProject/Counter.cs
@@ -0,0 +1,46 @@
+﻿
+public class Counter
+{
+    //parametr klasy - zmienna typu int o poziomie ochrony 'private' = nie bedzie dostepna z poziomu obiektu, tylko z poziomu klasy
+    private int Result = 0;
+
+
+    //konstruktory - są zawsze wywolywane przy tworzeniu obiektu klasy
+    //bezparametrowy - mozna tworzyc obiekt bez żadnych arg
+    //przyklad: Counter c1 = new Counter()
+    public Counter() {}
+
+    //zparametryzowany - mozna tworzyc obiekt z podaniem arg i cos z tym arg zrobic.. np przypisac go gdzies do zmiennej
+    //przyklad: Counter c1 = new Counter(5)
+    public Counter(int result)
+    {
+        Result = result;
+    }
+
+
+
+
+    //metody klasy - getter i setter do pobierania wartosci zmiennej prywatnej i do jej ustawiania
+    public int GetResult()
+    {
+        return Result;
+    }
+
+    public void SetResult(int x)
+    {
+        Result = x;
+    }
+
+
+
+
+    //metody klasy
+    public void Add(int x)
+    {
+        Result += x;
+    }
+    public void Subtract(int x)
+    {
+        Result -= x;
+    }
+}
---
+++ CounterProject/CounterProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterProject
+{
+    public class Program
+    {
+        public static int value = 0;
+
+        static void Main(string[] args)
+        {
+            Counter c1 = new Counter();
+
+            c1.SetResult(10);
+            c1.Add(5);
+
+            Console.WriteLine(c1.GetResult());
+            Console.ReadKey();
+        }
+    }
+}
